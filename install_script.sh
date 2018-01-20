5e82b3b71cea7c978c2d3a27b459d63619c051d6
NamelessPharaoh <34638873+NamelessPharaoh@users.noreply.github.com>
2018-01-20 11:42:56 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,24 +1,24 @@
 # Create dir for the tweak
-mkdir  -p $1.dir
+mkdir  -p deb
 OLD_PWD=$pwd
 
 # Extract
-#tar -xvf $1 -C $1.dir/
-cd $1.dir
-ar x ../$1
-tar -xvf data.tar.*
+tar -xzf $1 -C deb/
+cd deb
+ar x ../deb
+tar -xzf data.tar.*
 cd Library/
 
 # Sing using ldid (aka ldid2)
-# for bin in $(for file in $(find . | grep -Ev ".*.png" | grep -Ev ".*.plist" | grep -Ev ".*.txt" | grep -Ev ".*.sh"); do file $file; done | grep -v directory | awk '{print $1}' | sed 's@:@@' | sort -u); do echo $bin; ldid -S "$bin"; done
+# for bin in $(for file in $(find . | grep -Ev ".*.png" | grep -Ev ".*.plist" | grep -Ev ".*.txt" | grep -Ev ".*.sh"); do file $file; done | grep -v directory | awk '{print deb}' | sed 's@:@@' | sort -u); do echo $bin; ldid -S "$bin"; done
 
 # Copy to device
 if find . | grep MobileSubstrate; then
 scp -P 2222 MobileSubstrate/DynamicLibraries/* root@$2:/bootstrap/Library/SBInject/.
 fi
 if find . | grep PreferenceBundles; then
-scp -P 2222 -r PreferenceBundles/*.bundle root@$2:/bootstrap/Library/PreferenceBundles/.
-scp -P 2222 -r PreferenceLoader/Preferences/*.plist root@$2:/bootstrap/Library/PreferenceLoader/Preferences/.
+scp -P 2222 -r PreferenceBundles/*.bundle root@$2:/Library/PreferenceBundles/.
+scp -P 2222 -r PreferenceLoader/Preferences/*.plist root@$2:/Library/PreferenceLoader/Preferences/.
 fi
 if find . | grep "Application Support"; then
 cd "Application Support"
@@ -30,3 +30,5 @@ echo "Left overs:"
 echo "$left"
 
 cd $OLD_PWD
+rm -rf deb
+ssh root@$2 :  killall -9 SpringBoard
